15f58d941ecc2956889874a3e7e33e50d209ebfa
Tom Blackmore <tablackmore@gmail.com>
2016-09-07 09:16:32 +0200

    Added day2

---
+++ lectures/week3_javascript/day2/code_completed/js/numbers.js
@@ -0,0 +1,25 @@
+console.log('numbers');
+
+var decimal = 10;
+console.log(decimal + 9);
+
+var octal = 010;
+var octal2 = '010';
+console.log(octal);
+console.log(parseInt(octal2, 8));
+
+var hex = 0xBADA55;
+console.log(hex);
+
+var aString = '15kg';
+
+var aNumber = parseInt(aString, 10);
+
+console.log(aNumber);
+
+var anotherNumber = Number(aString);
+
+console.log(anotherNumber);
+
+var a = 'test';
+console.log(a);
