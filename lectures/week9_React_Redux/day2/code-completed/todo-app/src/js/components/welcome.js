e14819915e9f9dee6b7ad26398426ed04982f0c0
Tom Blackmore <tablackmore@gmail.com>
2016-10-18 15:36:52 +0200

    fixed structure

--- lectures/week9_React_Redux/day2/code-completed/todo-app/src/js/components/welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserUpdateForm = props => (
-  <div>
-    <h1>Howdy {props.username} welcome to the finest pet shop in the west</h1>
-  </div>
-);
-
-export default UserUpdateForm;
